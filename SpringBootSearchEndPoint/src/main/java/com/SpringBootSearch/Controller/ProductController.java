f4be1c693224963b2c30d351e4b2e47c9cf995e1
Dinesh Gupta <Dineshg000@gmail.com>
2023-04-05 13:55:22 +0530

    created ProductController

--- SpringBootSearchEndPoint/src/main/java/com/SpringBootSearch/Controller/ProductController.java
+++ SpringBootSearchEndPoint/src/main/java/com/SpringBootSearch/Controller/ProductController.java
@@ -1,7 +1,30 @@
 package com.SpringBootSearch.Controller;
 
+import com.SpringBootSearch.Entity.Product;
+import com.SpringBootSearch.Service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/products")
 public class ProductController {
 
+    private ProductService productService;
 
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
 
+    @GetMapping("search")
+    public ResponseEntity<List<Product>> searchProduct(@RequestParam ("query") String query){
+        return ResponseEntity.ok(productService.searchProduct(query));
+    }
+
+    @PostMapping()
+    public Product createproduct(@RequestBody Product product)
+    {
+        return productService.createProduct(product);
+    }
 }
