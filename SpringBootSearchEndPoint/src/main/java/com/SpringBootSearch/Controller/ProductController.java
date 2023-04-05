e554f45372dab41880baa3f38c39707c626b513d
Dinesh Gupta <Dineshg000@gmail.com>
2023-04-05 12:23:08 +0530

    added code for database connection

---
+++ SpringBootSearchEndPoint/src/main/java/com/SpringBootSearch/Controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.SpringBootSearch.Controller;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class ProductController {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sku;
+    private String name;
+    private String description;
+    private boolean active;
+    private String imageUrl;
+
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+    @UpdateTimestamp
+    private LocalDateTime dateUpdated;
+
+}
